5d59ca34d390c228f582a448583ede0a33dda39e
damin0912 <32475781+damin0912@users.noreply.github.com>
2019-05-29 05:18:51 +0900

    add gridView in weather part
    add gridView in weather part

--- app/src/main/java/com/example/o_k/weather_Activity.java
+++ app/src/main/java/com/example/o_k/weather_Activity.java
@@ -2,8 +2,13 @@ package com.example.o_k;
 
 import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -11,10 +16,16 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,6 +39,7 @@ import java.lang.ref.WeakReference;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -73,6 +85,19 @@ public class weather_Activity extends AppCompatActivity {
     private GpsInfo gps;
     private ProgressDialog progressDialog;
     private TextView textviewJSONText;
+
+    Spinner category;
+    GridView grid;
+
+    //카테고리 별 이미지 저장소
+    ArrayList<Bitmap> showImages = new ArrayList<Bitmap>();
+    ArrayList<Bitmap> allClothe = new ArrayList<Bitmap>();
+    ArrayList<Bitmap> cateTop = new ArrayList<Bitmap>();
+    ArrayList<Bitmap> cateBottom = new ArrayList<Bitmap>();
+    ArrayList<Bitmap> cateOuter = new ArrayList<Bitmap>();
+    ArrayList<Bitmap> cateEct = new ArrayList<Bitmap>();
+    //SQLite
+    DBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -156,7 +181,66 @@ public class weather_Activity extends AppCompatActivity {
         textviewJSONText = findViewById(R.id.weatherBox);
 
 
+        //Read Data
+        dbHelper = new DBHelper(weather_Activity.this, "MyDB.db", null, 1);
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        String sql = "SELECT * FROM closet";
+        Cursor cursor = db.rawQuery(sql, null);
+        byte[] blob;
+        String cate;
+        Bitmap bitmap;
+        while(cursor.moveToNext()){
+            cate = cursor.getString(1);
+            blob = cursor.getBlob(4);
+            bitmap = getImage(blob);
+            allClothe.add(bitmap);
+            if(cate.equals("상의")){
+                cateTop.add(bitmap);
+            }
+            else if (cate.equals("하의")){
+                cateBottom.add(bitmap);
+            }
+            else if(cate.equals("외투")){
+                cateOuter.add(bitmap);
+            }
+            else {
+                cateEct.add(bitmap);
+            }
+        }
 
+
+        //Spinner
+        category = (Spinner)findViewById(R.id.category_cloth);
+        category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if((parent.getItemAtPosition(position)).equals("전체")){
+                    showImages = allClothe;
+                }
+                if((parent.getItemAtPosition(position)).equals("상의")){
+                    showImages = cateTop;
+                }
+                if((parent.getItemAtPosition(position)).equals("하의")){
+                    showImages = cateBottom;
+                }
+                if((parent.getItemAtPosition(position)).equals("외투")){
+                    showImages = cateOuter;
+                }
+                if((parent.getItemAtPosition(position)).equals("기타")){
+                    showImages = cateEct;
+                }
+
+                //Create GridView
+                grid = findViewById(R.id.gridView);
+                weather_Activity.MyGridAdapter adapter = new weather_Activity.MyGridAdapter(weather_Activity.this);
+                grid.setAdapter(adapter);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         // GPS 정보를 보여주기 위한 이벤트 클래스 등록
         btnShowLocation.setOnClickListener(new View.OnClickListener() {
@@ -238,6 +322,10 @@ public class weather_Activity extends AppCompatActivity {
                             getApplicationContext(),
                             "당신의 위치 - \n위도: " + latitude + "\n경도: " + longitude + "\nAPI 사용 날짜 :" + base_date + "\nAPI 사용 시간 :" + base_time,
                             Toast.LENGTH_LONG).show();
+
+
+
+
                 } else {
                     // GPS 를 사용할수 없으므로
                     gps.showSettingsAlert();
@@ -455,5 +543,43 @@ public class weather_Activity extends AppCompatActivity {
         thread.start();
     }
 
+    public class MyGridAdapter extends BaseAdapter {
+        Context context;
+
+        public MyGridAdapter(Context c){
+            context = c;
+        }
+
+        @Override
+        public int getCount(){
+            return showImages.size();
+        }
+
+        @Override
+        public long getItemId(int index){
+            return showImages.indexOf(index);
+        }
+
+        @Override
+        public Object getItem(int index){
+            return showImages.indexOf(index);
+        }
+
+        @Override
+        public View getView(int index, View arg1, ViewGroup arg2){
+            ImageView imageView = new ImageView(context);
+            imageView.setPadding(0,0,0,0);
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+
+            imageView.setImageBitmap(showImages.get(index));
+
+            return imageView;
+        }
+    }
+
+    // convert from byte array to bitmap
+    public static Bitmap getImage(byte[] image) {
+        return BitmapFactory.decodeByteArray(image, 0, image.length);
+    }
 
 }
