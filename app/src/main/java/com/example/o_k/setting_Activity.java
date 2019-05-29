c2a7e2827905314792d22f5f4031ca08f75ca138
damin0912 <32475781+damin0912@users.noreply.github.com>
2019-05-29 15:25:10 +0900

    etc

--- app/src/main/java/com/example/o_k/setting_Activity.java
+++ app/src/main/java/com/example/o_k/setting_Activity.java
@@ -1,8 +1,13 @@
 package com.example.o_k;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
@@ -21,6 +26,11 @@ public class setting_Activity extends AppCompatActivity {
     Animation non_showMenu;
     RadioGroup rg;
 
+    NotificationManager notificationManager;
+    PendingIntent intent ;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -90,6 +100,62 @@ public class setting_Activity extends AppCompatActivity {
                 finish();
             }
         });
+
+        intent = PendingIntent.getActivity(this, 0,
+
+                new Intent(getApplicationContext(), MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+
+            Notification.Builder builder = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.refresh) // 아이콘 설정하지 않으면 오류남
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setContentTitle("알림 제목") // 제목 설정
+                .setContentText("알림 내용") // 내용 설정
+                .setTicker("한줄 출력") // 상태바에 표시될 한줄 출력
+                .setAutoCancel(true)
+                .setContentIntent(intent);
+
+
+
+        notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, builder.build());
+
+
+/**
+        Button sw = (Button)findViewById(R.id.clean_push);
+        //스위치의 체크 이벤트를 위한 리스너 등록
+        sw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                //Toast.makeText(setting_Activity.this, "체크상태 = " + isChecked, Toast.LENGTH_SHORT).show();
+
+                //if (isChecked == true) {
+                    intent = PendingIntent.getActivity(setting_Activity.this, 0,
+                            new Intent(getApplicationContext(), MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+
+                    Notification.Builder builder = new Notification.Builder(setting_Activity.this)
+                            .setSmallIcon(R.drawable.ic_launcher_background) // 아이콘 설정하지 않으면 오류남
+                            .setDefaults(Notification.DEFAULT_ALL)
+                            .setContentTitle("알림 제목") // 제목 설정
+                            .setContentText("알림 내용") // 내용 설정
+                            .setTicker("한줄 출력") // 상태바에 표시될 한줄 출력
+                            .setAutoCancel(true)
+                            .setContentIntent(intent);
+
+                    notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                    notificationManager.notify(0, builder.build());
+                    Log.i("Is checked is true is ", "근데 왜 출력안돼 ㅡㅡ");
+                }
+
+          //  }
+
+        });
+
+*/
+
 
 
         rg = (RadioGroup) findViewById(R.id.radGroupTheme);
